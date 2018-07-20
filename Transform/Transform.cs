a0535ac9eec7251b2e398543e61158e4d2a64986
Preetham D <preethamdbangera@gmail.com>
2018-07-20 14:09:24 +0530

    Using Generic Delegates

--- Transform/Transform.cs
+++ Transform/Transform.cs
@@ -6,9 +6,15 @@ namespace Transform
 {
     public static class Transform 
     {
-        public static void Map()
+        public static int[] Map(this int[] x,Func<int,int>condition)
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+            List<int> a = new List<int>();
+            foreach(int y in x)
+            {
+                a.Add(condition(y));
+            }
+            return a.ToArray();
         }
     }
 }
